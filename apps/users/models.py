856d3b8e6080c3ee43705a406dc1cc0dcb7d2641
MustafaMunir123 <mustafamunir10@gmail.com>
2025-05-22 00:38:47 +0500

    fix: correctuser model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -9,6 +10,7 @@ TYPE_CHOICES = (
 )
 
 class BaseUserModel(AbstractUser):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4, auto_created=True)
     email = models.EmailField(unique=True)
     type = models.CharField(
         max_length=10,
